cb699f39b02f97f264beab520e968bfb25b453b0
luizaspinelli <73395739+luizaspinelli@users.noreply.github.com>
2022-07-06 23:09:59 -0300

    Add files via upload

---
+++ T1/expande.py
@@ -0,0 +1,42 @@
+from nodo import Nodo
+from sucessor import sucessor
+
+# Exemplos para testar:
+nodo_pai = Nodo(estado="2_3541687")
+nodo_filho1 = Nodo(estado="_23541687", pai=nodo_pai, acao="esquerda")
+nodo_filho2 = Nodo(estado="2435_1687", pai=nodo_pai, acao="abaixo")
+nodo_filho3 = Nodo(estado="23_541687", pai=nodo_pai, acao="direita")
+
+
+def expande(nodo):
+    
+    listOfNodes = []
+    listOfSucc = sucessor(nodo.estado)         
+ 
+        
+    for acao, estado in listOfSucc:
+        nodoSucc = Nodo(estado=estado, pai=nodo, acao=acao)
+        listOfNodes.append(nodoSucc)        
+   
+    return listOfNodes
+
+
+
+def main():
+    sucessores = expande(nodo_pai)
+    
+    for nodo in sucessores:
+         print(nodo.estado, nodo.pai, nodo.acao)
+         print(Nodo.calcula_custo(nodo, nodo_pai))
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
