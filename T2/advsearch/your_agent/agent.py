345db9784a39be7778f48d385ee7c1e751eb0951
Juliane Alves <julianedarochaalves@gmail.com>
2022-08-09 21:53:26 -0300

    MinMax com poda alpha beta atualizado

--- T2/advsearch/your_agent/agent.py
+++ T2/advsearch/your_agent/agent.py
@@ -34,27 +34,31 @@ class MinMaxPodaAlphaBeta:
         self.board = the_board
         self.color = color
         self.depth = 4
+        self.opponent_color = self._opponent_color(color)
+
+    def _opponent_color(self, color):
+        return "B" if color == "W" else "W"
 
     def minmax(self):
-        value, action = self._max(self.board, -np.inf, np.inf, self.depth)
+        value, action = self._max(self.board, -np.inf, np.inf, self.depth, self.color)
         return value, action
 
-    def _max(self, board, alpha, beta, depth):
+    def _max(self, board, alpha, beta, depth, color):
         if depth == 0:
-            return self.calculate_points(board), None
+            return self.calculate_points(board, color), None
 
         value = -np.inf
         action = None
 
-        legal_moves = board.legal_moves(self.color)
+        legal_moves = board.legal_moves(color)
 
         if len(legal_moves) == 0:
             return 0, (-1, -1)
 
         for s in legal_moves:
             board_copy = deepcopy(board)
-            board_copy.process_move(s, self.color)
-            value = max(value, self._min(board_copy, alpha, beta, depth-1)[0])
+            board_copy.process_move(s, color)
+            value = max(value, self._min(board_copy, alpha, beta, depth-1, self._opponent_color(color))[0])
             action = s
             alpha = max(alpha, value)
 
@@ -63,22 +67,22 @@ class MinMaxPodaAlphaBeta:
 
         return alpha, action
 
-    def _min(self, board, alpha, beta, depth):
+    def _min(self, board, alpha, beta, depth, color):
         if depth == 0:
-            return self.calculate_points(board), None
+            return self.calculate_points(board, color), None
 
         value = np.inf
         action = None
 
-        legal_moves = board.legal_moves(self.color)
+        legal_moves = board.legal_moves(color)
 
         if len(legal_moves) == 0:
             return 0, (-1, -1)
 
         for s in legal_moves:
             board_copy = deepcopy(board)
-            board_copy.process_move(s, self.color)
-            value = min(value, self._max(board_copy, alpha, beta, depth-1)[0])
+            board_copy.process_move(s, color)
+            value = min(value, self._max(board_copy, alpha, beta, depth-1, self._opponent_color(color))[0])
             action = s
             alpha = min(beta, value)
 
@@ -87,7 +91,7 @@ class MinMaxPodaAlphaBeta:
 
         return beta, action
 
-    def calculate_points(self, board):
+    def calculate_points(self, board, color):
         """
         Metodo que calcula os pontos dado o estado atual do board
         """
